db134f4290801d1681fc97e057758c8bcd15da2f
maximkfd <ipust13@gmail.com>
2016-08-01 15:25:05 +0300

    test

--- app/src/main/java/com/akvone/dlcifmo/MainActivity.java
+++ app/src/main/java/com/akvone/dlcifmo/MainActivity.java
@@ -135,7 +135,9 @@ public class MainActivity extends AppCompatActivity
             startActivity(new Intent(getApplicationContext(),LoginActivity.class));
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        if (drawer != null) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
         return true;
     }
 }
